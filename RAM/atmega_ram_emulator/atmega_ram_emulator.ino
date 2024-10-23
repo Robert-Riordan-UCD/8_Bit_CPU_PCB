32d8a6470a1a3a3d67bf69a7912ee022cd09ab9e
Robert-Riordan-UCD <r.riordan.ie@gmail.com>
2024-10-23 15:42:56 +1100

    Atmega uses interrupts instead of polling to monitor for write signal to prevent missing the signal

--- RAM/atmega_ram_emulator/atmega_ram_emulator.ino
+++ RAM/atmega_ram_emulator/atmega_ram_emulator.ino
@@ -1,19 +1,12 @@
-uint8_t memory[16]; // Using 16 8-bit values because there are no 4-bit data types. Bits 7 to 4 are unused on each byte.
+#include "PinChangeInterrupt.h"
+
+uint8_t memory[16] = {}; // Using 16 8-bit values because there are no 4-bit data types. Bits 7 to 4 are unused on each byte.
 
 int address_pins[4] = {A0, A1, A2, A3};
 int data_output_pins[4] = {5, 4, 3, 2};
 int data_input_pins[4] = {7, 6, 9, 10};
 
 int n_write_enable_pin = 8; // Write when low. Read when high
-
-void setup() {
-  for (int i = 0; i < 4; i++) {
-    pinMode(address_pins[i], INPUT);
-    pinMode(data_output_pins[i], INPUT); // High impedence when not in use
-    pinMode(data_input_pins[i], INPUT);
-  }
-  pinMode(n_write_enable_pin, INPUT);
-}
 
 void writeToRAM () {
   for (int i = 0; i < 4; i++) { // Set output to high impedence when not in use
@@ -37,10 +30,17 @@ void readFromRAM() {
   }
 }
 
-void loop() {
-  if (digitalRead(n_write_enable_pin) == LOW) { // Write to RAM
-    writeToRAM();
-  } else { // Read from RAM
-    readFromRAM();
+void setup() {
+  for (int i = 0; i < 4; i++) {
+    pinMode(address_pins[i], INPUT);
+    pinMode(data_output_pins[i], INPUT); // High impedence when not in use
+    pinMode(data_input_pins[i], INPUT);
   }
+  pinMode(n_write_enable_pin, INPUT);
+
+  attachPCINT(digitalPinToPCINT(n_write_enable_pin), writeToRAM, FALLING); // Write to RAM on falling edge of write enable
+}
+
+void loop() {
+  readFromRAM();
 }
